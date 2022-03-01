6cffc0c7cecac39d2a130a886eaafa20b61e9fee
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-03-01 20:35:40 +0530

    update package

--- client/src/components/MyJobs/INF/Inf.js
+++ client/src/components/MyJobs/INF/Inf.js
@@ -181,10 +181,6 @@ const Inf = () => {
   };
 
   // ------------------------------------------------------------
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ba2356b6a3ecca9d3277eb1a7873fd4a76274e1
   const selectionData = {
     Total_Number_Of_Rounds: 0,
     Number_Of_Offers: 0,
