f8639b0ac9ac7a74aa6f5d81d58e09546105cfa0
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2022-03-02 21:08:33 +0530

    CoursesAdded

--- client/src/components/Courses/Courses.js
+++ client/src/components/Courses/Courses.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 
 import './styles.css';
@@ -8,13 +9,22 @@ const Courses = () => {
   const Navigate=useNavigate();
   return (
     <div>
-    <div>Courses</div>
-    <button onClick={() => Navigate('/courses/btech')}>Btech</button>
-    <button onClick={() => Navigate('/courses/fiveyear')}>Five Year Courses</button>
-    <button onClick={() => Navigate('/courses/twoyearmba')}>Two Year MBA</button>
-    <button onClick={() => Navigate('/courses/twoyearmsc')}>Two Year MSC</button>
-    <button onClick={() => Navigate('/courses/twoyearmtech')}>Two Year M.Tech</button>
-    <button onClick={() => Navigate('/courses/threeyearmsc')}>Three Year MSC</button>
+      <div className="user_profile">
+      <div className="profile_header">
+          <h2>Courses</h2>
+   
+      </div>
+      <div className='buttons'>
+      <div className='bt' id='btnfirst'><Button variant="contained" onClick={() => Navigate('/courses/btech')}>Btech</Button></div>
+    <div className='bt'><Button variant="contained" onClick={() => Navigate('/courses/fiveyear')}>Five Year Courses</Button></div>
+    <div className='bt'><Button variant="contained" onClick={() => Navigate('/courses/twoyearmba')}>Two Year MBA</Button></div>
+    <div className='bt'><Button variant="contained" onClick={() => Navigate('/courses/twoyearmsc')}>Two Year MSC</Button></div>
+    <div className='bt'><Button variant="contained" onClick={() => Navigate('/courses/twoyearmtech')}>Two Year M.Tech</Button></div>
+    <div className='bt'><Button variant="contained" onClick={() => Navigate('/courses/threeyearmsc')}>Three Year MSC</Button></div>
+    </div>
+      </div>
+    
+
   
   
     </div>
