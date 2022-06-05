ab9456ab5a1489cf74cec28fc545972725844314
unknown <krittikabarnwal2014@gmail.com>
2022-06-06 00:47:32 +0530

    admin contact

--- server/src/api/routes/v1/contact.route.js
+++ server/src/api/routes/v1/contact.route.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
 
-const { protect, authorizeRoles } = require('../../middlewares/auth');
+const { protect, authorizeRoles } = require("../../middlewares/auth");
 
-const { sendContactData, changeContactStatus, deleteContactData,getAllContacts } = require('../../controllers/contact.controller');
+const {
+  sendContactData,
+  changeContactStatus,
+  deleteContactData,
+  getAllContacts,
+} = require("../../controllers/contact.controller");
 
 const router = express.Router();
 
-router.post('/', sendContactData);
+router.post("/", sendContactData);
 
-router.put('/:id', protect, authorizeRoles, changeContactStatus);
+router.put("/:id", changeContactStatus);
 
-router.delete('/:id', protect, authorizeRoles, deleteContactData);
+router.delete("/:id", protect, authorizeRoles, deleteContactData);
 
-router.get('/all', protect, getAllContacts);
+router.get("/all", protect, getAllContacts);
 
 module.exports = router;
