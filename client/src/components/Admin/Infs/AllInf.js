3a870dfeb455e228bb7dc39fac839767cec15c47
unknown <krittikabarnwal2014@gmail.com>
2022-06-15 15:23:54 +0530

    card

--- client/src/components/Admin/Infs/AllInf.js
+++ client/src/components/Admin/Infs/AllInf.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Dropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-} from 'reactstrap';
-import { Button } from '@mui/material';
-import { FaSearch } from 'react-icons/fa';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import Loading from '../../Loading/Loading';
-import './../Jobs/styles.css';
+} from "reactstrap";
+import { Button } from "@mui/material";
+import { FaSearch } from "react-icons/fa";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Loading from "../../Loading/Loading";
+import "./../Jobs/styles.css";
 import {
   fetchAllInf,
   getAllInf,
   getAllJobs,
   searchInf,
   searchInfByPattern,
-} from '../../../api/index';
+} from "../../../api/index";
 
 const AllInf = () => {
   const [jobs, setJobs] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const [pageNo, setPageNo] = useState('1');
+  const [pageNo, setPageNo] = useState("1");
 
   const [search, setSearch] = useState();
   const Navigate = useNavigate();
 
-  let user = JSON.parse(localStorage.getItem('user'));
+  let user = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
-    if (!user || user.isemailVerified === false || user.role !== 'Admin') {
-      Navigate('/auth');
+    if (!user || user.isemailVerified === false || user.role !== "Admin") {
+      Navigate("/auth");
     }
   }, [Navigate, user]);
 
@@ -54,7 +54,7 @@ const AllInf = () => {
       setJobs(response.data.jobs);
     } catch (error) {
       setIsLoading(false);
-      Navigate('/badgateway');
+      Navigate("/badgateway");
     }
   };
 
@@ -69,13 +69,13 @@ const AllInf = () => {
         try {
           response = await fetchAllInf(12, pageNo);
         } catch (error) {
-          Navigate('/badgateway');
+          Navigate("/badgateway");
         }
       } else {
         try {
           response = await searchInf(search, 12, pageNo);
         } catch (error) {
-          Navigate('/badgateway');
+          Navigate("/badgateway");
         }
       }
       setJobs(response.data.jobs);
@@ -83,10 +83,10 @@ const AllInf = () => {
     fetchINFs();
   }, [search]);
 
-  const [dropdownOpen, setDropdownOpen] = useState('');
+  const [dropdownOpen, setDropdownOpen] = useState("");
   const handletoggle = (id) => () => {
     if (dropdownOpen === id) {
-      setDropdownOpen(() => '');
+      setDropdownOpen(() => "");
     } else {
       setDropdownOpen(() => id);
     }
@@ -94,69 +94,61 @@ const AllInf = () => {
 
   return (
     <>
-      <div className='admin_company'>
-        <div className='admin_company_header d-flex  justify-content-between'>
+      <div className="admin_company">
+        <div className="admin_company_header d-flex  justify-content-between">
           <h1>All INF</h1>
           <div>
-            <div className='input-group d-flex'>
-              <div className='form-outline'>
+            <div className="input-group d-flex">
+              <div className="form-outline">
                 <input
-                  type='search'
-                  id='form1'
-                  className='form-control'
-                  placeholder='Type Company Name'
-                  name='search'
+                  type="search"
+                  id="form1"
+                  className="form-control"
+                  placeholder="Type Company Name"
+                  name="search"
                   value={search}
                   onChange={handleOnChange}
                 />
               </div>
-              <Button variant='contained'>
+              <Button variant="contained">
                 <FaSearch />
               </Button>
             </div>
           </div>
         </div>
 
-        <div className='job_items'>
+        <div className="job_items">
           {jobs &&
             jobs.map((job) => (
               <div
-                className='job_card'
+                className="job_card"
                 key={job._id}
-                style={{ display: 'inline-block' }}
+                style={{ display: "inline-block" }}
               >
+                {console.log(job)}
                 <div
-                  className='badge'
-                  style={{ backgroundColor: !job.isIntern && 'red' }}
+                  className="badge"
+                  style={{ backgroundColor: !job.isIntern && "red" }}
                 >
-                  <h6>{job.isIntern ? 'Intern' : 'FTE'}</h6>
+                  <h6>{job.isIntern ? "Intern" : "FTE"}</h6>
                 </div>
-                <div className='card_content'>
-                  <div className='content_heading'>
+                <div className="card_content  ps-2">
+                  <div className="content_heading">
                     <h4>{job?.Company_Overview?.CO_Name_Of_The_Company}</h4>
                   </div>
-                  <div className='content_text'>
+                  <div className="content_text">
                     <h5>
                       <span>Duration: </span>
                       {job?.Intern_Profile?.IP_Internship_Duration}
                     </h5>
                     <h5>
-                      {job.isIntern ? (
-                        <>
-                          <span>Mode</span>:{' '}
-                          {job?.Intern_Profile?.IP_Mode_Of_Internship}
-                        </>
-                      ) : (
-                        <>
-                          <span>Place of posting</span>:{' '}
-                          {job?.Job_Details?.JD_Place_Of_Posting}
-                        </>
-                      )}
+                      <span>Designation: </span>
+                      {job?.Intern_Profile?.IP_Job_Designation}
                     </h5>
                     <h5>
                       {job.isIntern ? (
                         <>
-                          <span>Stipend</span>:{' '}
+                          <span>Stipend</span>:{" "}
                           {job?.Stipend_Details?.SD_Salary_Per_Month}
                         </>
                       ) : (
@@ -166,31 +158,26 @@ const AllInf = () => {
                       )}
                     </h5>
                     <h5>
-                      <span>Submitted On:</span>:{' '}
+                      <span>Submitted On:</span>:{" "}
                       {job.updatedAt.slice(8, 10) +
-                        '/' +
+                        "/" +
                         job.updatedAt.slice(5, 7) +
-                        '/' +
+                        "/" +
                         job.updatedAt.slice(0, 4)}
                     </h5>
 
-                    <div
-                      className='d-flex align-items-center'
-                      style={{
-                        position: 'absolute',
-                      }}
-                    >
+                    <div className="d-flex align-items-center">
                       <div>
-                        <button className='secondary_btn py-1'>
+                        <button className="secondary_btn py-1">
                           <a
                             href={job.adminPreviewLink}
-                            style={{ textDecoration: 'none', color: 'inherit' }}
+                            style={{ textDecoration: "none", color: "inherit" }}
                           >
                             View Job
                           </a>
                         </button>
                       </div>
-                      <div className='my-2 ms-3 ps-2'>
+                      <div className="my-2 ms-3 ps-2">
                         <Dropdown
                           isOpen={dropdownOpen === job._id}
                           toggle={handletoggle(job._id)}
@@ -201,8 +188,8 @@ const AllInf = () => {
                               <a
                                 href={job.studentDownloadLink}
                                 style={{
-                                  textDecoration: 'none',
-                                  color: 'inherit',
+                                  textDecoration: "none",
+                                  color: "inherit",
                                 }}
                               >
                                 For Student
@@ -213,8 +200,8 @@ const AllInf = () => {
                               <a
                                 href={job.adminDownloadLink}
                                 style={{
-                                  textDecoration: 'none',
-                                  color: 'inherit',
+                                  textDecoration: "none",
+                                  color: "inherit",
                                 }}
                               >
                                 For Admin
@@ -223,16 +210,6 @@ const AllInf = () => {
                           </DropdownMenu>
                         </Dropdown>
                       </div>
-                      {/* <button className="secondary_btn">
-                      {" "}
-                      <a
-                        href={job.downloadLink}
-                        style={{ textDecoration: "none", color: "inherit" }}
-                      >
-                        {" "}
-                        Download
-                      </a>{" "}
-                    </button> */}
                     </div>
                   </div>
                 </div>
@@ -242,8 +219,8 @@ const AllInf = () => {
         <Stack spacing={1}>
           <Pagination
             count={20}
-            color='primary'
-            style={{ margin: '3rem auto' }}
+            color="primary"
+            style={{ margin: "3rem auto" }}
             onChange={handlePageChange}
           />
         </Stack>
